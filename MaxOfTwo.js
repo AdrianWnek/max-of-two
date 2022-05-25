e1ce37846fcfdac27e8c2f2ce79a61e656782ca4
AdrianWnek <103371387+AdrianWnek@users.noreply.github.com>
2022-05-25 15:40:24 -0500

    Add files via upload

---
+++ MaxOfTwo.js
@@ -0,0 +1,11 @@
+var x = 1 ;
+var y = 2 ;
+if (x == y) {
+    console.log('The values are identical');
+} else {
+    if (x > y) {
+        console.log('X')
+    } else {
+        console.log('Y')
+    }
+};
